c3b0527cc048b979cc48403acd4b51ab75608ca8
Amanda Leaders <alsleaders@gmail.com>
2019-05-30 16:54:11 -0400

    changed link

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+
 class HelloWorld extends Component {
   render() {
     return (
       <section>
         <h1>Hello, Hector! love spaceballs</h1>
-        <Link to="/">Home</Link>
       </section>
     )
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 class Home extends Component {
   render() {
-    return <div />
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home}>
+              Home
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    )
   }
 }
 
