25e389b1c019603698255e9e4590dba4162bf14c
Amanda Leaders <alsleaders@gmail.com>
2019-05-30 17:02:44 -0400

    nav bar

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import Home from '../pages/Home'
 
 class HelloWorld extends Component {
   render() {
     return (
-      <section>
-        <h1>Hello, Hector! love spaceballs</h1>
-      </section>
+      <>
+        <nav>
+          <Home />
+        </nav>
+        <section>
+          <h1>Hello, Hector! love spaceballs</h1>
+        </section>
+      </>
     )
   }
 }
