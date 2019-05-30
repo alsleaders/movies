25bf443e040f33dca17ebdce357e2cf79818088b
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-05-30 17:34:09 -0400

    fixed issue in Header

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import Movie_Night from './Components/Images/Movie_Night.png'
 class Header extends Component {
   render() {
-    return <section className='MovieLogoHeader'>
-<img src={Movie_Night} alt="Movie Logo Header" />
-
-    </section
+    return (
+      <section className="MovieLogoHeader">
+        <img src={Movie_Night} alt="Movie Logo Header" />
+      </section>
+    )
   }
 }
 
