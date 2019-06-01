e7356b348dd37ffdc4d63117a2fad172061c0931
Amanda Leaders <alsleaders@gmail.com>
2019-06-01 15:31:11 -0400

    movieFetch showing on homepage

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Home from './pages/Home.js'
 import Header from './pages/Header.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { styles } from 'ansi-colors'
+import MovieFetch from './pages/MovieFetch.js'
 
 class App extends Component {
   render() {
     return (
       <>
         <div className="Maincontainer">
-          <Home />
           <Header />
+          <Home />
           <section>
             <h1>Hello, Hector! love spaceballs</h1>
+            <MovieFetch />
           </section>
         </div>
       </>
