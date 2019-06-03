1ec7be5b41689680d686c9b183071aeb18f363a3
Amanda Leaders <alsleaders@gmail.com>
2019-06-02 21:35:13 -0400

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './pages/Header.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { styles } from 'ansi-colors'
 import MovieFetch from './pages/MovieFetch.js'
+import Footer from './pages/Footer'
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <div className="Maincontainer">
           <Header />
           <Home />
+          <Footer />
         </div>
       </>
     )
---
+++ src/pages/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          This product uses the TMDb API but is not endorsed or certified by
+          TMDb.
+        </p>
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" />
+      </div>
+    )
+  }
+}
+
+export default Footer
